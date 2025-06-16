30cd4c223d253f37fbd4028ec5b2da951022c552
parkjongbok <jongbok654@naver.com>
2025-06-16 17:05:59 +0900

    Step01 에 MainClass055 추가함

--- step01_datatye/src/test/main/MainClass05.java
+++ step01_datatye/src/test/main/MainClass05.java
@@ -2,9 +2,20 @@ package test.main;
 //String type
 public class MainClass05 {
 public static void main(String[] args) {
+	//김구라 라는 String type 개게를 heap 영역(사물형)에 만들고 그 참족밧을 변수에 담기
 	String name="김구라";
+	//name 안에 있는 참조값을 tmp 변수에 복사해 주기
 	String tmp=name;
+	//원숭이 라는 String type 객체를 heap 영역에 만들고 그 참조값을 name 변수에 덮어쓰기
 	name="원숭이";
+	//name 변수를 배우기 (null은 참조 데이터 type 이 담길 수 있는 빈 공간을 의미)
 	name=null;
+	//변수안에 참조값이 들어가는게 아니고 실제 값이 들어가는 기본 데이터 type
+	int num=10;
+	double num2=10.1;
+	boolean isRun=true;
+	//변수 안에 참조값이 들어가는 참조 데이터 type만 String
+	String yourName="해골";
+	
 }
 }
